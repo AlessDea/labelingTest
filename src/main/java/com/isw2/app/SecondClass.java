bbf5ae897106baa65c9cae15e84016554b934e17
AlessDea <ale.flanker97@gmail.com>
2022-05-13 22:56:24 +0200

    added SecondClass constructor

---
+++ src/main/java/com/isw2/app/SecondClass.java
@@ -0,0 +1,13 @@
+package com.isw2.app;
+
+public class SecondClass {
+    int n1;
+    int n2;
+    int n3;
+
+    public SecondClass(){
+        this.n1 = 1;
+        this.n2 = 2;
+        this.n3 = 3;
+    }
+}
