3e24bd6ce17773b4e7e7688440df3390eefaf4ec
AlessDea <ale.flanker97@gmail.com>
2022-05-27 17:55:19 +0200

    modified SecondClass

--- src/main/java/com/isw2/app/SecondClass.java
+++ src/main/java/com/isw2/app/SecondClass.java
@@ -3,11 +3,11 @@ package com.isw2.app;
 public class SecondClass {
     int n1;
     int n2;
-    int n3;
+    String s;
 
     public SecondClass(){
         this.n1 = 1;
         this.n2 = 2;
-        this.n3 = 3;
+        this.s = new String("Hello world!");
     }
 }
